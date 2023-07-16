a7349d1761369edf0e9d215bdb51f893efdf15fd
Rijn Buve <rijn.buve@tomtom.com>
2023-07-16 16:09:04 +0200

    1.3.0

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,4 +1,4 @@
-export const TOMTOM_CHATBOT_UI_VERSION = '1.2.3';
+export const TOMTOM_CHATBOT_UI_VERSION = '1.3.0';
 
 export const OPENAI_DEFAULT_SYSTEM_PROMPT =
   process.env.OPENAI_DEFAULT_SYSTEM_PROMPT ||
