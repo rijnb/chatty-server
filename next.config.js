6e181e4e756a9a4b60099adcf603123060dd86b9
Rijn Buve <rijn.buve@tomtom.com>
2023-07-16 16:39:19 +0200

    Remove static config

--- next.config.js
+++ next.config.js
@@ -4,11 +4,6 @@ const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   i18n,
   reactStrictMode: true,
-  output: 'export',
-  distDir: '_static',
-  images: {
-    unoptimized: true
-  },
 
   webpack(config, { isServer, dev }) {
     config.experiments = {
