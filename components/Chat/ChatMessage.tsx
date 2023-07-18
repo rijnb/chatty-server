62449da1dceafd2cd32a0c78141ed68038ec66b4
Rijn Buve <rijn@buve.nl>
2023-07-18 19:59:18 +0200

    User input shown as Markdown

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -189,7 +189,43 @@ export const ChatMessage: FC<Props> = memo(({ message, messageIndex, onEdit }) =
                 </div>
               ) : (
                 <div className="prose whitespace-pre-wrap dark:prose-invert flex-1">
-                  {message.content}
+                  <MemoizedReactMarkdown
+                      className="prose dark:prose-invert flex-1"
+                      remarkPlugins={[remarkGfm, remarkMath]}
+                      rehypePlugins={[rehypeMathjax]}
+                      components={{
+                        code({ node, inline, className, children, ...props }) {
+                          return (
+                              <code className={className} {...props}>
+                                {children}
+                              </code>
+                          );
+                        },
+                        table({ children }) {
+                          return (
+                              <table className="border-collapse border border-black px-3 py-1 dark:border-white">
+                                {children}
+                              </table>
+                          );
+                        },
+                        th({ children }) {
+                          return (
+                              <th className="break-words border border-black bg-gray-500 px-3 py-1 text-white dark:border-white">
+                                {children}
+                              </th>
+                          );
+                        },
+                        td({ children }) {
+                          return (
+                              <td className="break-words border border-black px-3 py-1 dark:border-white">
+                                {children}
+                              </td>
+                          );
+                        },
+                      }}
+                  >
+                    {`${message.content}`}
+                  </MemoizedReactMarkdown>
                 </div>
               )}
 
@@ -222,12 +258,10 @@ export const ChatMessage: FC<Props> = memo(({ message, messageIndex, onEdit }) =
                       if (children[0] == '▍') {
                         return <span className="animate-pulse cursor-default mt-1">▍</span>
                       }
-
                       children[0] = (children[0] as string).replace("`▍`", "▍")
                     }
 
                     const match = /language-(\w+)/.exec(className || '');
-
                     return !inline ? (
                       <CodeBlock
                         key={Math.random()}
