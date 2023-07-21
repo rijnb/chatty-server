d471404cfd126d20724dc2bd5bc60c3f6fb131f8
Rijn Buve <rijn@buve.nl>
2023-07-21 13:11:34 +0200

    Cleaned up header

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -442,26 +442,28 @@ export const Chat = memo(({stopConversationRef}: Props) => {
                     <>
                       <div
                           className="sticky top-0 z-10 flex justify-center border border-b-neutral-300 bg-neutral-100 py-2 text-sm text-neutral-500 dark:border-none dark:bg-[#444654] dark:text-neutral-200">
-                        {t("Model")}: {selectedConversation?.model.name} | {t("Temp")}
-                        : {selectedConversation?.temperature} |
+                        {t("Model")}: {selectedConversation?.model.name} |
                         <button
                             className="ml-2 cursor-pointer hover:opacity-50"
                             onClick={handleSettings}
                         >
                           <IconSettings size={18}/>
                         </button>
-                        <button
-                            className="ml-2 cursor-pointer hover:opacity-50"
-                            onClick={onClearAll}
-                        >
-                          <IconClearAll size={18}/>
-                        </button>
-                        <button
-                            className="ml-2 cursor-pointer hover:opacity-50"
-                            onClick={onScreenshot}
-                        >
-                          <IconScreenshot size={18}/>
-                        </button>
+                        {
+                        //!! TODO re-add clear all and screenshot
+                        // <button
+                        //     className="ml-2 cursor-pointer hover:opacity-50"
+                        //     onClick={onClearAll}
+                        // >
+                        //   <IconClearAll size={18}/>
+                        // </button>
+                        // <button
+                        //     className="ml-2 cursor-pointer hover:opacity-50"
+                        //     onClick={onScreenshot}
+                        // >
+                        //   <IconScreenshot size={18}/>
+                        // </button>
+                        }
                       </div>
                       {showSettings && (
                           <div
