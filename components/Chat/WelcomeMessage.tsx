b474a768f10bd91c5e377d3014408991f0f6f901
Rijn Buve <rijn.buve@tomtom.com>
2023-07-26 10:16:07 +0200

    Fixed welcome message

--- components/Chat/WelcomeMessage.tsx
+++ components/Chat/WelcomeMessage.tsx
@@ -2,7 +2,6 @@ import {FC, useContext} from "react"
 
 import HomeContext from "@/pages/api/home/home.context"
 
-
 export interface Props {}
 
 export const WelcomeMessage: FC<Props> = () => {
@@ -17,14 +16,14 @@ export const WelcomeMessage: FC<Props> = () => {
         <div className="mx-auto flex flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-blue-800 dark:text-blue-400">CHATTY</div>
           <div className="text-center text-xl font-bold text-yellow-700 dark:text-yellow-100">
-            Conversational Host At TomTom...Yes!
+            A better interface for OpenAI GPT-3/GPT-4
           </div>
           <div className="text-center text-gray-700 dark:text-gray-400">
             <div className="mb-2 text-gray-800 dark:text-gray-200">Chatty was developed by Rijn Buve.</div>
             Chatty is 100% unaffiliated with OpenAI. It is based on <span className="italic"> chatbot-ui </span>
-            by Mckay Wrigley and includes features from many contributors to <span className="italic">chatbot-ui</span>.
-            Many thanks to all. Chatty is licensed under the MIT license.
+            by Mckay Wrigley and includes features from many contributors.
           </div>
+          <div className="text-center text-gray-600 dark:text-gray-400">- = o O o = -</div>
         </div>
       </div>
     </div>
