4e4ad4ef3c86e94d57c9fa6a6ac3895517aeaa35
Rijn Buve <rijn.buve@tomtom.com>
2023-07-26 10:07:47 +0200

    Fixed welcome message

--- components/Chat/WelcomeMessage.tsx
+++ components/Chat/WelcomeMessage.tsx
@@ -12,9 +12,9 @@ export const WelcomeMessage: FC<Props> = () => {
   } = useContext(HomeContext)
 
   return (
-    <div className="mx-auto flex flex-col space-y-5 md:space-y-10 px-3 pt-5 md:pt-12 sm:max-w-[600px]">
+    <div className="mx-auto flex flex-col space-y-5 md:space-y-10 px-3 pt-5 md:pt-12">
       <div className="relative flex-1 overflow-hidden bg-white dark:bg-[#343541]">
-        <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
+        <div className="mx-auto flex flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-blue-800 dark:text-blue-400">CHATTY</div>
           <div className="text-center text-xl font-bold text-yellow-700 dark:text-yellow-100">
             Conversational Host At TomTom...Yes!
