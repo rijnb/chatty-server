a87067062a14c4058652f3e7c3ad506ae3f5dd52
Rijn Buve <rijn.buve@tomtom.com>
2023-07-27 10:17:02 +0200

    Fixed variable prompt

--- components/Chat/PromptVariableModal.tsx
+++ components/Chat/PromptVariableModal.tsx
@@ -89,7 +89,7 @@ export const PromptVariableModal: FC<Props> = ({prompt, promptVariables, onSubmi
               ref={index === 0 ? nameInputRef : undefined}
               className="mt-1 w-full rounded-lg border border-neutral-500 px-4 py-2 text-neutral-900 shadow focus:outline-none dark:border-neutral-800 dark:border-opacity-50 dark:bg-[#40414F] dark:text-neutral-100"
               style={{resize: "none"}}
-              placeholder={`Enter a value for ${variable.key}...`}
+              placeholder={`${variable.key}`}
               value={variable.value}
               onChange={(e) => handleChange(index, e.target.value)}
               rows={3}
