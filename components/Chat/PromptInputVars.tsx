a0d0ade688e9be29f6b7206e1630da327a50de00
Rijn Buve <rijn@buve.nl>
2023-08-15 10:53:51 +0200

    Added drop zone (not done yet)

--- components/Chat/PromptInputVars.tsx
+++ components/Chat/PromptInputVars.tsx
@@ -1,7 +1,8 @@
-import {FC, KeyboardEvent, useEffect, useRef, useState} from "react"
+import {ChangeEvent, KeyboardEvent, useEffect, useRef, useState} from "react"
 import {isKeyboardEnter} from "@/utils/app/keyboard"
 import {Prompt} from "@/types/prompt"
 import {ModalDialog} from "@/components/ModalDialog"
+
 
 interface Props {
   prompt: Prompt
@@ -10,7 +11,7 @@ interface Props {
   onCancel: () => void
 }
 
-export const PromptInputVars: FC<Props> = ({prompt, promptVariables, onSubmit, onCancel}) => {
+export const PromptInputVars = ({prompt, promptVariables, onSubmit, onCancel}: Props) => {
   const [updatedPromptVariables, setUpdatedPromptVariables] = useState<{key: string; value: string}[]>(
     promptVariables
       .map((promptVariable) => ({key: promptVariable, value: ""}))
@@ -18,6 +19,22 @@ export const PromptInputVars: FC<Props> = ({prompt, promptVariables, onSubmit, o
   )
 
   const nameInputRef = useRef<HTMLTextAreaElement>(null)
+
+  const handleFileDrop = (e: ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files
+    if (files) {
+      // @ts-ignore
+      ;[...files].forEach((file) => {
+        const reader = new FileReader()
+        reader.onload = (e) => {
+          const fileContent = e.target?.result
+          console.log(`fileContent="${fileContent}"`)
+          // Process the fileContent as needed.
+        }
+        reader.readAsText(file)
+      })
+    }
+  }
 
   const handleChange = (index: number, value: string) => {
     setUpdatedPromptVariables((prev) => {
@@ -28,8 +45,7 @@ export const PromptInputVars: FC<Props> = ({prompt, promptVariables, onSubmit, o
   }
 
   const handleSubmit = () => {
-    onSubmit(updatedPromptVariables.map((variable) =>
-      variable.value === "" ? " " : variable.value))
+    onSubmit(updatedPromptVariables.map((variable) => (variable.value === "" ? " " : variable.value)))
   }
 
   const handleKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
@@ -46,38 +62,59 @@ export const PromptInputVars: FC<Props> = ({prompt, promptVariables, onSubmit, o
   }, [])
 
   return (
-    <ModalDialog
-      className="dark:border-netural-400 inline-block max-h-[400px] transform overflow-y-auto rounded-lg border border-gray-300 bg-white px-4 pb-4 pt-5 text-left align-bottom shadow-xl transition-all dark:bg-[#202123] sm:my-8 sm:max-h-[600px] sm:w-full sm:max-w-lg sm:p-6 sm:align-middle"
-      onClose={onCancel}
-    >
-      <div role="dialog" onKeyDown={handleKeyDown}>
+    <>
+      <input type="file" id="file-input" className="hidden" multiple onChange={handleFileDrop} />
+
+      <ModalDialog
+        className="dark:border-netural-400 inline-block max-h-[400px] transform overflow-y-auto rounded-lg border border-gray-300 bg-white px-4 pb-4 pt-5 text-left align-bottom shadow-xl transition-all dark:bg-[#202123] sm:my-8 sm:max-h-[600px] sm:w-full sm:max-w-lg sm:p-6 sm:align-middle"
+        onClose={onCancel}
+      >
         <div className="mb-4 text-xl font-bold text-black dark:text-neutral-200">{prompt.name}</div>
         <div className="mb-4 text-sm text-black dark:text-neutral-200">{prompt.description}</div>
 
-          {updatedPromptVariables.map((variable, index) => (
-              <div className="mb-4" key={index}>
-                <div className="mb-2 text-sm font-bold text-neutral-200">{variable.key}:</div>
+        {updatedPromptVariables.map((variable, index) => (
+          <div className="mb-4" key={index}>
+            <div className="mb-2 text-sm font-bold text-neutral-200">{variable.key}:</div>
 
-                <textarea
-                    ref={index === 0 ? nameInputRef : undefined}
-                    className="mt-1 w-full rounded-lg border border-neutral-500 px-4 py-2 text-neutral-900 shadow focus:outline-none dark:border-neutral-800 dark:border-opacity-50 dark:bg-[#40414F] dark:text-neutral-100"
-                    style={{resize: "none"}}
-                    placeholder={`${variable.key}...`}
-                    value={variable.value}
-                    onChange={(e) => handleChange(index, e.target.value)}
-                    rows={5}
-                />
+            {(index === updatedPromptVariables.length - 1) && (variable.key.endsWith("@DROP")) ? (
+              <div role="dialog" onKeyDown={handleKeyDown}>
+                <label
+                  htmlFor="file-input"
+                  className="drop-zone flex h-10 w-full cursor-pointer items-center justify-center rounded-lg border-2 text-center text-neutral-600"
+                  onDragOver={(e) => e.preventDefault()}
+                  onDrop={(e) => {
+                    e.preventDefault()
+                    if (e.dataTransfer.files) {
+                      const fileInput = document.getElementById("file-input") as HTMLInputElement
+                      fileInput.files = e.dataTransfer.files
+                      handleFileDrop({target: fileInput} as ChangeEvent<HTMLInputElement>)
+                    }
+                  }}
+                >
+                  Drop your files here (or click to select files)
+                </label>{" "}
+                <button
+                  className="mt-6 w-full rounded-lg border border-neutral-500 px-4 py-2 text-neutral-900 shadow hover:bg-neutral-100 focus:outline-none dark:border-neutral-800 dark:border-opacity-50 dark:bg-white dark:text-black dark:hover:bg-neutral-300"
+                  onClick={handleSubmit}
+                >
+                  Submit
+                </button>
               </div>
-          ))}
-
-        <button
-          className="mt-6 w-full rounded-lg border border-neutral-500 px-4 py-2 text-neutral-900 shadow hover:bg-neutral-100 focus:outline-none dark:border-neutral-800 dark:border-opacity-50 dark:bg-white dark:text-black dark:hover:bg-neutral-300"
-          onClick={handleSubmit}
-        >
-          Submit
-        </button>
-      </div>
-    </ModalDialog>
+            ) : (
+              <textarea
+                ref={index === 0 ? nameInputRef : undefined}
+                className="mt-1 w-full rounded-lg border border-neutral-500 px-4 py-2 text-neutral-900 shadow focus:outline-none dark:border-neutral-800 dark:border-opacity-50 dark:bg-[#40414F] dark:text-neutral-100"
+                style={{resize: "none"}}
+                placeholder={`${variable.key}...`}
+                value={variable.value}
+                onChange={(e) => handleChange(index, e.target.value)}
+                rows={5}
+              />
+            )}
+          </div>
+        ))}
+      </ModalDialog>
+    </>
   )
 }
 
