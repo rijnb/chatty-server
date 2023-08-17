36f4f71259e134caff86d5b58371a9e0cde97427
Rijn Buve <rijn@buve.nl>
2023-08-17 09:23:03 +0200

    Changed system font, slightly narrower for titles in side bars

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,12 @@ import {appWithTranslation} from "next-i18next"
 import {ThemeProvider} from "next-themes"
 import type {AppContext, AppInitialProps, AppProps} from "next/app"
 import App from "next/app"
-import {Inter} from "next/font/google"
+import {PT_Sans} from "next/font/google"
 import {UnlockProvider} from "@/components/UnlockCode"
 import "@/styles/globals.css"
 
 
-const inter = Inter({subsets: ["latin"]})
+const inter = PT_Sans({subsets: ["latin"], weight: "400"})
 
 type ChattyAppProps = {
   isProtected: boolean
