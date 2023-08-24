68f2abdd0a6e20d5b6a849b1a69010273cc3df9a
Oleksii Kulyk <oleksii.kulyk@tomtom.com>
2023-08-24 14:46:26 +0200

    scroll down button

---
+++ components/Chat/ScrollDownButton.tsx
@@ -0,0 +1,26 @@
+import {IconArrowDown} from "@tabler/icons-react"
+import React from "react"
+
+interface Props {
+  container: any
+}
+
+const ScrollDownButton = ({container}: Props) => {
+  const handleScrollToBottom = () => {
+    container.current?.scrollTo({
+      top: container.current.scrollHeight,
+      behavior: "smooth"
+    })
+  }
+
+  return (
+    <button
+      className="flex h-7 w-7 items-center justify-center rounded-full bg-neutral-300 text-gray-800 shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:text-neutral-200"
+      onClick={handleScrollToBottom}
+    >
+      <IconArrowDown size={18} />
+    </button>
+  )
+}
+
+export default ScrollDownButton
