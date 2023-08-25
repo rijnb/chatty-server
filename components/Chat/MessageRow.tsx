ecfc81c64806eff2a329ba24106d0997361f08cd
Oleksii Kulyk <oleksii.kulyk@tomtom.com>
2023-08-25 12:30:30 +0200

    message components

---
+++ components/Chat/MessageRow.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+interface Props {
+  className: string
+  icon: React.ReactNode
+  children: React.ReactNode
+}
+
+const MessageRow = ({className, icon, children}: Props) => {
+  return (
+    <div className={className} style={{overflowWrap: "anywhere"}}>
+      <div className="relative m-auto flex max-w-2xl gap-2 p-4 px-0 py-4 text-base">
+        <div className="min-w-[40px] text-right font-bold">{icon}</div>
+        <div className="prose mt-[-2px] w-full dark:prose-invert">{children}</div>
+      </div>
+    </div>
+  )
+}
+
+export default MessageRow
